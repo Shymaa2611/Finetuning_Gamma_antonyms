7077c2d5ae1e257602c18b15aa03e4c166597175
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-14 20:41:36 +0300

    update

--- model.py
+++ model.py
@@ -1,31 +1,25 @@
-from transformers import BitsAndBytesConfig, AutoModelForCausalLM, AutoTokenizer
-from peft import LoraConfig
 import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+from peft import LoraConfig
 
 def model_Quantization():
-    bnb_config = BitsAndBytesConfig(
-        load_in_8bit=True,
-        bnb_8bit_quant_type="nf4",
-        bnb_8bit_compute_dtype=torch.float16
-    ) 
-
     token = "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"  
     model_name = "google/gemma-2b-it"
     tokenizer = AutoTokenizer.from_pretrained(model_name, use_auth_token=token)
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name, 
-        quantization_config=bnb_config, 
-        use_auth_token=token
+    model = AutoModelForCausalLM.from_pretrained(model_name, use_auth_token=token)
+    model = torch.quantization.quantize_dynamic(
+        model,
+        {torch.nn.Linear}, 
+        dtype=torch.qint8   
     )
     
     return model, tokenizer
 
 def Lora_Configuration():
-
     lora_config = LoraConfig(
         r=8,
         target_modules=["q_proj", "o_proj", "k_proj", "v_proj", "gate_proj", "up_proj", "down_proj"],
         task_type="CAUSAL_LM",
     )
-    
     return lora_config
+
