e386351eb30149c8515d5878bd2554b579e04761
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-14 19:56:00 +0300

    update

--- inference.py
+++ inference.py
@@ -19,7 +19,7 @@ def generate_inference(model, tokenizer, input_text, max_length=50):
     return generated_text
 
 if __name__=="__main__":
-  checkpoint_dir = "checkpoint"
+  checkpoint_dir = "/kaggle/working/Finetuning_Gamma_antonyms/outputs/checkpoint-300"
   model, tokenizer = load_model_and_tokenizer(checkpoint_dir)
   input_text = "good"
   generated_text = generate_inference(model, tokenizer, input_text)
