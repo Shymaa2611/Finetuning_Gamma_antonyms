d1180b193f44650637617b1a4c739669d90959dd
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-14 21:09:34 +0300

    update

--- training.py
+++ training.py
@@ -6,7 +6,7 @@ def Training_Arguments():
         per_device_train_batch_size=2, 
         gradient_accumulation_steps=4,
         warmup_steps=100,  
-        max_steps=200    
+        max_steps=200,    
         learning_rate=1e-5,  
         fp16=True,
         logging_steps=10, 
