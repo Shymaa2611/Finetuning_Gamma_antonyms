f9b56fd52c2bf6cc0286be4650d22e01b99eca17
Shymaa2611 <shymaamedhatahmed99@gmail.com>
2024-09-14 21:02:31 +0300

    update

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 from training import Trainer
 from dataset import get_data
-#from model import model_Quantization,Lora_Configuration
-#from evaluate import evaluate_model
-from model import model_with_lora_and_quantization
-def run(train_loader,model):
-    trainer=Trainer(train_loader,model)
+from model import model_Quantization,Lora_Configuration
+#from evaluate import evaluate
+def run(train_loader,model,lora_config):
+    trainer=Trainer(train_loader,model,lora_config)
     trainer.train()
-    #eval_results = evaluate_model(trainer, test_loader)
-    #print("Evaluation Results:", eval_results)
     
 
 
 
 if __name__=="__main__":
-    model,tokenizer=model_with_lora_and_quantization()
-    #lora_config=Lora_Configuration()
+    model,tokenizer=model_Quantization()
+    lora_config=Lora_Configuration()
     train_data, test_data, train_loader, test_loader=get_data('data/antonyms.csv',tokenizer)
-    run(train_loader,model)
+    run(train_loader,model,lora_config)
 
 
 
